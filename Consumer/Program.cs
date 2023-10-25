df3807fe31d8637943d4c07d3dcd3d9ca440b77b
Bex <beksen00@gmail.com>
2023-10-25 10:43:00 +0200

    .

---
+++ Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    var messag = Encoding.UTF8.GetString(body);
+    Console.WriteLine(messag);
+};
+
+channel.BasicConsume("demo-queue", true, consumer);
+Console.ReadLine();
+
---
+++ Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+var message = new { Name = "Producer", Message = "Hello" };
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+channel.BasicPublish("", "demo-queue", null, body);
+
+channel.QueueDeclare("test-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+var message2 = new { Name = "Producer", Message = "test" };
+var body2 = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message2));
+
+channel.BasicPublish("", "test-queue", null, body2);
